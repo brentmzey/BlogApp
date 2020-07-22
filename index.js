a5efd98f6c8b03893eb0a633153f002648cd6d81
Brent Zey <brentmzey4795@gmail.com>
2020-07-22 13:51:16 -0500

    Functionality to add new posts and view all posts

--- index.js
+++ index.js
@@ -3,19 +3,61 @@ const bodyParser = require("body-parser"),
   express = require("express"),
   app = express();
 
-// Set up mongoose
-mongoose.connect("mongodb://localhost:27017/restful_blog");
+// APP CONFIG
+mongoose
+  .connect("mongodb://localhost:27017/restful_blog", {
+    // need to use the localhost port of 27017 since our local instance of Mongo runs/listens here
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to 'restful_blog' DB!"))
+  .catch((error) => console.log(error.message));
 // Set up some express primitives
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set up SCHEMA and MODEL
+// Mongoose SCHEMA & MODEL CONFIG
 const blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
   created: { type: Date, default: Date.now },
+});
+const Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful ROUTES
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+// INDEX Route
+app.get("/blogs", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+// NEW Route
+app.get("/blogs/new", (req, res) => {
+  res.render("new");
+});
+
+// CREATE Route
+app.post("/blogs", (req, res) => {
+  // Create blog
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) {
+      res.render("new");
+    } else {
+      // Then redirect to the index
+      res.redirect("/blogs");
+    }
+  });
 });
 
 // Set up server for app to listen
--- readme.md
+++ readme.md
@@ -4,3 +4,16 @@
 - Create the Blog model
 - Add INDEX route and template
 - Add simple nav bar
+
+# Basic Layout
+
+- Add header and footer partials
+- Include Semantic UI
+- Add in a simple Navbar
+
+# Adding in some "C" - create in CRUD
+
+- Add NEW route
+- Add NEW template
+- Add CREATE route
+- Add CREATE template
